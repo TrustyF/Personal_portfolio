4d00397797972228a667e2af75671a8838a6e018
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2024-08-02 15:32:12 -0700

    Small layout fixes, changed video embed settings. Novembre page

--- src/components/generic/VimeoEmbed.vue
+++ src/components/generic/VimeoEmbed.vue
@@ -13,7 +13,7 @@ let props = defineProps({
 <template>
   <div style="padding:56.25% 0 0 0;position:relative;">
     <iframe
-        :src="`https://player.vimeo.com/video/${id}?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`"
+        :src="`https://player.vimeo.com/video/${id}?autoplay=1&loop=1&title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479`"
         frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
         style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
   </div>
--- src/project_pages/pages/novembre.vue
+++ src/project_pages/pages/novembre.vue
@@ -2,6 +2,7 @@
 import {inject, onMounted, watch, computed} from "vue";
 import ProjectPageTemplate from "@/components/project/ProjectPageTemplate.vue";
 import SoftwareTag from "@/components/project/SoftwareTag.vue";
+import VimeoEmbed from "@/components/generic/VimeoEmbed.vue";
 
 let props = defineProps({
   test: {
@@ -19,52 +20,16 @@ function get_image(path) {
 </script>
 
 <template>
-  <project-page-template :project_name="proj_name">
+  <project-page-template :project_name="proj_name" :image_loader="false">
+
     <template #content>
 
-      <div class="single_image">
-        <div style="display: grid;  grid-template-columns: repeat(2, 1fr);gap: 0">
-          <img :src="get_image('terr1.jpg')" class="grid_image" alt="">
-          <img :src="get_image('terr2.jpg')" class="grid_image" alt="">
+      <div class="vimeo_video">
+        <div style="display: grid;grid-template-columns: 3fr 1fr;">
+          <vimeo-embed id="994261948"/>
+          <vimeo-embed id="994261959"/>
         </div>
-        <h5>Procedural height field + automated texture generation</h5>
-      </div>
-
-      <div class="single_image">
-        <div style="display: grid;  grid-template-columns: repeat(4, 1fr);gap: 0">
-          <img :src="get_image('skull1_crop.jpg')" class="grid_image" alt="">
-          <img :src="get_image('skull2_crop.jpg')" class="grid_image" alt="">
-          <img :src="get_image('skull3_crop.jpg')" class="grid_image" alt="">
-          <img :src="get_image('skull4_crop.jpg')" class="grid_image" alt="">
-        </div>
-        <h5>Integrating skull as temperature mask</h5>
-      </div>
-
-      <div class="single_image">
-        <div style="display: grid;  grid-template-columns: repeat(3, 1fr);gap: 0">
-          <img :src="get_image('traj.jpg')" class="grid_image" alt="">
-          <img :src="get_image('vel1.jpg')" class="grid_image" alt="">
-          <img :src="get_image('vel2.jpg')" class="grid_image" alt="">
-        </div>
-        <h5>Trajectory automatically finds nearest valid impact point. Generates impact velocities for debris and
-          sand</h5>
-      </div>
-
-      <div class="single_image">
-        <div style="display: grid;  grid-template-columns: repeat(2, 1fr);gap: 0">
-          <img :src="get_image('def2.jpg')" class="grid_image" alt="">
-          <img :src="get_image('def1.jpg')" class="grid_image" alt="">
-        </div>
-        <h5>Deforms heightfield on impact</h5>
-      </div>
-
-      <div class="single_image">
-        <div style="display: grid;  grid-template-columns: repeat(3, 1fr);gap: 0">
-          <img :src="get_image('trail1.jpg')" class="grid_image" alt="">
-          <img :src="get_image('trail2.jpg')" class="grid_image" alt="">
-          <img :src="get_image('temp2.jpg')" class="grid_image" alt="">
-        </div>
-        <h5>Post-impact, trail smoke</h5>
+        <h5 style="text-align: center;padding-top: 10px">Final comp - Messaging setup demo</h5>
       </div>
 
     </template>
@@ -81,10 +46,7 @@ function get_image(path) {
   gap: 10px;
 }
 
-.youtube_video {
-  /*outline: 1px solid red;*/
-
-  /*justify-items: center;*/
+.vimeo_video {
   justify-content: center;
   width: 100%;
 }

2bd92e969b1d04533025ce47f57401ff16ea2ae1
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2024-09-04 15:11:38 -0700

    Fixed api key

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@ import {log_event} from "@/scripts/log_events.js";
 
 let route = useRoute()
 let is_mobile = ref(0)
-let geo_api = 'a0a1177ef494406a877242c1c4e3d396'
 function check_mobile() {
   let in_width = window.innerWidth;
   if (in_width > 1000) is_mobile.value = 0
@@ -16,7 +15,8 @@ function check_mobile() {
 }
 
 async function log_geo(){
-  let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${geo_api}`;
+  let key = import.meta.env.VITE_GEO_API
+  let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${key}`;
   let geo = await fetch(url)
       .then(resp => resp.json())
       .then(json => {
